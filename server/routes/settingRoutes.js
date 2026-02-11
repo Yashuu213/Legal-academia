308da0f13f5409e2abc0d6e3d97863e470fb6ade
Yashuu213 <sharmapyashy@gmail.com>
2026-02-11 13:49:23 +0530

    fix middleware import mismatch causing crash

--- server/routes/settingRoutes.js
+++ server/routes/settingRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const settingController = require('../controllers/settingController');
-const { protect, admin } = require('../middleware/authMiddleware');
+const { verifyToken, verifyAdmin } = require('../middleware/authMiddleware');
 
 // Public route to get a setting (like whatsapp link)
 router.get('/:key', settingController.getSetting);
 
 // Admin only route to update a setting
-router.put('/:key', protect, admin, settingController.updateSetting);
+router.put('/:key', verifyToken, verifyAdmin, settingController.updateSetting);
 
 module.exports = router;
